e15b84455cae5d04ab9ef556f53617c36563b652
Nabila Febriyanti <55932660+fabbbn@users.noreply.github.com>
2021-04-17 15:43:14 +0700

    Update training.py

--- training.py
+++ training.py
@@ -8,39 +8,39 @@ Created on Thu Apr  8 11:55:14 2021
 from gensim.models.word2vec import Text8Corpus
 from gensim.models.phrases import Phrases
 # Cleaned corpus
-sentences = Text8Corpus('train_text.txt')
+sentences = Text8Corpus('training_dataset/train_text.txt')
 first_sentence = next(iter(sentences))
 print(first_sentence[:10])
 
 phrase_model1 = Phrases(sentences, min_count=1, threshold=0.3)
-phrase_model1.save('phrase_model1.pkl')
+phrase_model1.save('models/phrase_model1.pkl')
 print("model with threshold=0.3, scoring='default' created")
 
 phrase_model2 = Phrases(sentences, min_count=1, threshold=0.5)
-phrase_model2.save('phrase_model2.pkl')
+phrase_model2.save('models/phrase_model2.pkl')
 print("model with threshold=0.5, scoring='default' created")
 
 phrase_model3 = Phrases(sentences, min_count=1, threshold=1)
-phrase_model3.save('phrase_model3.pkl')
+phrase_model3.save('models/phrase_model3.pkl')
 print("model with min+count=1, threshold=1 created")
 
 print("3 models to cleaned text have been made")
 
 # # Uncleaned Corpus
-sentences1 = Text8Corpus('raw.txt')
+sentences1 = Text8Corpus('training_dataset/raw.txt')
 first_sentence = next(iter(sentences1))
 print(first_sentence[:10])
 
 phrase_model6 = Phrases(sentences1, min_count=1, threshold=0.3)
-phrase_model6.save('phrase_model6.pkl')
+phrase_model6.save('models/phrase_model6.pkl')
 print("model with threshold=0.3, scoring='default' created")
 
 phrase_model7 = Phrases(sentences1, min_count=1, threshold=0.5)
-phrase_model7.save('phrase_model7.pkl')
+phrase_model7.save('models/phrase_model7.pkl')
 print("model with threshold=0.1, scoring='default' created")
 
 phrase_model8 = Phrases(sentences1, min_count=1, threshold=1)
-phrase_model8.save('phrase_model8.pkl')
+phrase_model8.save('models/phrase_model8.pkl')
 print("model with threshold=1, scoring='default' created")
 
 print("3 models to uncleaned text have been made")
