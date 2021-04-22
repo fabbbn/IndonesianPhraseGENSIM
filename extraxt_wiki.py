b39311881dfc1597c784789e2cdf1cc383eff69d
Nabila Febriyanti <55932660+fabbbn@users.noreply.github.com>
2021-04-22 20:57:57 +0700

    Update extraxt_wiki.py
    fix directories

--- extraxt_wiki.py
+++ extraxt_wiki.py
@@ -11,31 +11,30 @@ from gensim.models.phrases import Phrases
 import re
 import os
 from clean_text import *
-# DATA 1:  punctuation and other symbols pattern removed
-# clean = open('train_text.txt', 'w')
-# root = "C:/Users/user/Documents/SEMESTER6/NLP/Project/program/text"
-# dirs = os.listdir(root)
-# for dir in dirs:
-#     print(dir)
-#     extracts = os.listdir(root+'/'+dir+'/')
-#     for ext in extracts:
-#         print('\t' + ext)
-#         path = root + '/' + dir + '/' + ext
-#         # print(path)
-#         f1 = open(path, 'r', encoding='utf-8').read()
-# #         # print(f1)
-#         for line in f1.split('\n'):
-#             if ('<doc id' not in line) and ('</doc' not in line):
-#                 cleanline = clean_text(line)
-#                 # print(cleanline)
-#                 clean.write(cleanline + "\n")
 
-# clean.close()
-# print("file train_text.txt has been created")
+# DATA 1:  punctuation and other symbols pattern removed
+clean = open('./training_dataset/train_text.txt', 'w')
+root = "./text"
+dirs = os.listdir(root)
+for dir in dirs:
+    print(dir)
+    extracts = os.listdir(root+'/'+dir+'/')
+    for ext in extracts:
+        print('\t' + ext)
+        path = root + '/' + dir + '/' + ext
+        f1 = open(path, 'r', encoding='utf-8').read()
+        for line in f1.split('\n'):
+            if ('<doc id' not in line) and ('</doc' not in line):
+                cleanline = clean_text(line)
+                # print(cleanline)
+                clean.write(cleanline + "\n")
+
+clean.close()
+print("file train_text.txt has been created")
 
 # DATA 2:  punctuation and other symbols pattern removed
-clean = open('raw.txt', 'w')
-root = "C:/Users/user/Documents/SEMESTER6/NLP/Project/program/text"
+clean = open('./training_dataset/raw.txt', 'w')
+root = "./text"
 dirs = os.listdir(root)
 # print(dirs)
 for dir in dirs:
@@ -44,12 +43,10 @@ for dir in dirs:
     for ext in extracts:
         print('\t' + ext)
         path = root + '/' + dir + '/' + ext
-        # print(path)
         f1 = open(path, 'r', encoding='ascii', errors='ignore').read()
-        # print(f1)
         for line in f1.split('\n'):
             if ('<doc id' not in line) and ('</doc' not in line):
                 clean.write(str(line) + "\n")
 
 clean.close()
-# print("file raw.txt has been created")
+print("file raw.txt has been created")
